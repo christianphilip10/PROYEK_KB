59b14fcebcd01ebb7c053da425a953cf3061812d
Nicholas Nathanael Suhendar <104911087+Konicho46@users.noreply.github.com>
2023-06-07 14:32:11 +0700

    Update invaders.py
    Tambah komen-komen

--- invaders.py
+++ invaders.py
@@ -9,32 +9,33 @@ if __name__ == '__main__':
     # Create Game object
     game.InvadersGame()
 
-    # Load backgrounds
+    # Load backgrounds pada aset bernama final_space.jpg
     wall_sprite = sge.gfx.Sprite(name='final_space')
     layers = [sge.gfx.BackgroundLayer(wall_sprite,0 ,0)]
     background = sge.gfx.Background(layers, sge.gfx.Color('black'))
 
-    # Create objects
+    # Menggenerate warna objek
+    # Kraken nantinya akan menghasil kan berbagai macam warna random yang sudah dibuat
     colors_random = [(255, 0, 0),  # Red
                      (0, 0, 255),  # Blue
                      (0, 255, 0),  # Green
                      (255, 255, 0),  # Yellow
                      (255, 165, 0),  # Orange
-                     (128, 0, 128)]  # Purple]
+                     (128, 0, 128)]  # Purple
     invaders = [objects.Invader(colors=random.choice(colors_random)) for _ in range(6)]
     player = objects.Player()
 
-    # Player is always the first object
+    # PLayer menjadi objek yang pertama
     obj = [player] + invaders
 
     # Create room
     sge.game.start_room = game.GameRoom(obj, background=background)
 
-    # Remove the mouse to increase performance by avoiding collision detection
+    # Mouse dibuat invisible atau tak terliat yang berguna untuk mengatasi collision
     sge.game.mouse.visible = False
     sge.game.start_room.remove(sge.game.mouse)
 
-    # Here we go!
+    # Game telah dimulai! Yey!
     sge.game.start()
 
 
